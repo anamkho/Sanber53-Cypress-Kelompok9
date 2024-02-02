a5c5da9438d80f312764f9aee56725a408260fc2
anamkho <anamkho99@gmail.com>
2024-02-03 00:45:01 +0700

    Register Test Cases

--- cypress/e2e/3-web/saucedemo/login.cy.js
+++ cypress/e2e/3-web/saucedemo/login.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Functionality', () => {
+  it('Verify Failed Login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('anamkho')
+    cy.get('[name="password"]').type('123456')
+    cy.get('.submit-button.btn_action').click()
+    cy.get('[data-test="error"]').should('be.visible')
   })
 })
