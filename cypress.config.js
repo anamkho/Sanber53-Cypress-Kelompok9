9b2d5519fea0274b665929cf555ec90b1dbe10cb
anamkho <anamkho99@gmail.com>
2024-02-03 15:58:47 +0700

    update POM

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://magento.softwaretestingboard.com/customer/account/create/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  defaultCommandTimeout : 1234
+  defaultCommandTimeout: 1234,
+  viewportHeight: 636,
+  viewportWidth: 989
 });
