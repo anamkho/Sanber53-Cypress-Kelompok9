16fb5532b3c2ae613113b33bc4a8b033fb6dc0cd
ffiadharma <cut.nailaff@gmail.com>
2024-02-04 21:13:20 +0700

    edit account with fixtures

--- cypress/e2e/4-web/softwaretestingboard/edit-acc.cy.js
+++ cypress/e2e/4-web/softwaretestingboard/edit-acc.cy.js
@@ -1,5 +1,6 @@
 import editAcc from "../../../support/pageObject/editAcc"
 const editAccData = require('../../../fixtures/editAccData.json')
+const hft = "N"
 
 describe('template spec', () => {
   beforeEach(() => {
