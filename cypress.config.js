5b7ed0ba37cc9bf0c1eb0af33e757cbda4cab60d
ffiadharma <cut.nailaff@gmail.com>
2024-02-04 20:44:39 +0700

    edit account

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 2024,
+  viewportHeight: 636,
+  viewportWidth: 989,
+
   e2e: {
+    baseUrl: "https://magento.softwaretestingboard.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
