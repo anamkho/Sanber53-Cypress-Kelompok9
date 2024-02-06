15ab55dfdff56ae385752f970c5cc2cd99cf03a8
LutfiArrasyid <arrasyidlutfi@gmail.com>
2024-02-06 19:12:50 +0700

    fix

--- cypress/e2e/4-web/softwaretestingboard/view-updatecart.cy.js
+++ cypress/e2e/4-web/softwaretestingboard/view-updatecart.cy.js
@@ -1,4 +1,5 @@
 import signinPage from "../../../support/pageObject/signinPage"
+const signInData = require('../../../fixtures/signInData.json')
 import productPage from "../../../support/pageObject/productPage"
 import cartPage from "../../../support/pageObject/cartPage"
 const cart = require('../../../fixtures/cart.json')
@@ -8,38 +9,60 @@ describe('Choose Product Functionality', () => {
     beforeEach(() => {
         cy.visit('')
       })
-    it('verify view update cart - not login cart empty ', () => {
+    it('verify view update cart - cart empty - not login', () => {
       cy.get('.showcart').click()
       cy.get('.subtitle').should('contain.text','You have no items in your shopping cart')
     })
 
-    it.only('verify view update cart - not login add cart', () => {
+    it('verify view update cart - add cart - not login', () => {
       cy.get(productPage.inptPrd).type(cart["namaProduk"])
-      cy.get(productPage.srcBtn).click()
-      cy.get(productPage.prdCart).click()
-      cy.get(productPage.inptClr).click()
-      cy.get(productPage.inptSize).click()
+      cy.get(productPage.srcBtn).click({force: true})
+      cy.get(productPage.prdCart).click({force: true})
+      cy.get(productPage.inptClr).click({force: true})
+      cy.get(productPage.inptSize).click({force: true})
       cy.get(productPage.inptQty).clear().type(cart["qty"])
       productPage.clickAddCart()
       cy.get(cartPage.shwCart).click({force: true})
-      cy.get(cartPage.viewUpdtBtn).click({force: true})
-      //cy.url().should('contain', 'https://magento.softwaretestingboard.com/checkout/cart/')
+      cy.get(':nth-child(7) > .secondary > .action > span').click({force: true})   
     })
 
-    it('verify view update cart - user have login ', () => {
+    it.only('verify view update cart - update cart - user login', () => {
+      cy.contains('Sign In').click()
+      cy.get(signinPage.email).type(signInData["valid-data"]["valid-email"])
+      cy.get(signinPage.pwd).type(signInData["valid-data"]["valid-pwd"])
+      signinPage.clickSignIn()
+      cy.url().should('contain','https://magento.softwaretestingboard.com/')
+      cy.get(cartPage.shwCart).click({force: true})
+      cy.get(':nth-child(7) > .secondary > .action > span').click({force: true})
+      cy.url().should('contain','https://magento.softwaretestingboard.com/checkout/cart/')
+      cartPage.clickUpdtShpCart()
+    })
+
+    it('verify view update cart - edit cart - user login', () => {
         cy.contains('Sign In').click()
         cy.get(signinPage.email).type(signInData["valid-data"]["valid-email"])
         cy.get(signinPage.pwd).type(signInData["valid-data"]["valid-pwd"])
         signinPage.clickSignIn()
         cy.url().should('contain','https://magento.softwaretestingboard.com/')
-        cy.get(cartPage.shwCart).click()
+        cy.get(cartPage.shwCart).click({force: true})
         cy.get(':nth-child(7) > .secondary > .action > span').click({force: true})
         cy.url().should('contain','https://magento.softwaretestingboard.com/checkout/cart/')
-        cy.get(cartPage.editItem).click()
-        cy.get(cartPage.editSize).click()
-        cy.get(cartPage.editClr).click()
+        cy.get(cartPage.editItem).click({force: true})
+        cy.get(cartPage.editSize).click({force: true})
+        cy.get(cartPage.editClr).click({force: true})
         cartPage.clickUpdtCart()
+    })
 
+    it('verify view update cart - delete cart - user login', () => {
+      cy.contains('Sign In').click()
+      cy.get(signinPage.email).type(signInData["valid-data"]["valid-email"])
+      cy.get(signinPage.pwd).type(signInData["valid-data"]["valid-pwd"])
+      signinPage.clickSignIn()
+      cy.url().should('contain','https://magento.softwaretestingboard.com/')
+      cy.get(cartPage.shwCart).click({force: true})
+      cy.get(':nth-child(7) > .secondary > .action > span').click({force: true})
+      cy.url().should('contain','https://magento.softwaretestingboard.com/checkout/cart/')
+      cy.get(cartPage.deleteBtn).click({force: true})
     })
 
 })
